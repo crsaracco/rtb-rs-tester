bf25eba447cef9a11549137b30daf179fd142a8f
Charles Saracco <crsaracco@gmail.com>
2018-12-17 20:07:11 -0500

    Add an editor window that immediately crashes the plugin since rtb-rs::platform::x11::create_platform_window() is unimplemented

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,19 @@
+use rtb_rs::Window;
+use rtb_rs::WindowDimensions;
+
 pub struct Editor {
+    window: Window,
 }
 
 impl Editor {
     pub fn new() -> Self {
-        Self {}
+        let window_dimensions = WindowDimensions {
+            width: 1000,
+            height: 1000,
+        };
+
+        Self {
+            window: Window::open_under(None, window_dimensions, "rtb-rs test window"),
+        }
     }
 }
